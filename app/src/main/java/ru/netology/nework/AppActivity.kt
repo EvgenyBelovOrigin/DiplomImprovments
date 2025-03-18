e4b54ab5aab651302e629c28375692d9c6006b26
Evgeny <e.belov@inbox.ru>
2025-03-18 16:13:09 +0300

    NAV DEPENDENCIES added

--- app/src/main/java/ru/netology/nework/AppActivity.kt
+++ app/src/main/java/ru/netology/nework/AppActivity.kt
@@ -1,14 +1,13 @@
 package ru.netology.nework
 
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 
-class MainActivity : AppCompatActivity() {
+class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_app)
 
     }
 }
