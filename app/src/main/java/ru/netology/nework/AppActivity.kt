570ad89e7d3f5358255a836afe4faac47575189c
Evgeny <e.belov@inbox.ru>
2025-03-19 19:13:33 +0300

    API_KEY added

--- app/src/main/java/ru/netology/nework/AppActivity.kt
+++ app/src/main/java/ru/netology/nework/AppActivity.kt
@@ -2,12 +2,12 @@ package ru.netology.nework
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import java.util.Properties
 
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
-
     }
 }
