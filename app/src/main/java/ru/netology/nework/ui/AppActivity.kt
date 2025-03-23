402bd398674bbd202793024cdf08134ec72b885e
Evgeny <e.belov@inbox.ru>
2025-03-23 19:36:29 +0300

    AppBar added

--- app/src/main/java/ru/netology/nework/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nework/ui/AppActivity.kt
@@ -1,7 +1,12 @@
 package ru.netology.nework.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.netology.nework.R
 
@@ -9,5 +14,42 @@ import ru.netology.nework.R
 class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        addMenuProvider(
+            object : MenuProvider {
+                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                    menuInflater.inflate(R.menu.auth_menu, menu)
+                    menu.setGroupVisible(R.id.authenticated, false)
+                    menu.setGroupVisible(R.id.unauthenticated, true)
+//                    viewModel.data.observe(this@AppActivity) {
+//                        menu.setGroupVisible(R.id.authenticated, viewModel.authenticated)
+//                        menu.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
+//
+//                    }
+                }
+
+                override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
+                    when (menuItem.itemId) {
+                        R.id.signin -> {
+                            findNavController(R.id.nav_host_fragment).navigate(R.id.signInFragment)
+                            true
+                        }
+
+                        R.id.signup -> {
+                            findNavController(R.id.nav_host_fragment).navigate(R.id.signUpFragment)
+                            true
+                        }
+
+//                        R.id.signout -> {
+////                            appAuth.clear()
+//                            true
+//                        }
+
+                        else -> false
+                    }
+
+
+            }
+        )
     }
 }
