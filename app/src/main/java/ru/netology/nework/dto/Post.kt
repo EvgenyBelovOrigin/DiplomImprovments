dfd92c5a67bfef0d3c47a42fa74a26736a8a265c
Evgeny <e.belov@inbox.ru>
2025-03-23 14:31:26 +0300

    Post dto added

---
+++ app/src/main/java/ru/netology/nework/dto/Post.kt
@@ -0,0 +1,48 @@
+package ru.netology.nework.dto
+
+data class Post(
+    val id: Int,
+    val authorId: Int,
+    val author: String,
+    val authorJob: String,
+    val authorAvatar: String?,
+    val content: String,
+    val published: String,
+    val coords: Coordinates? = null,
+    val link: String? = null,
+    val mentionIds: List<Int>? = null,
+    val mentionedMe: Boolean,
+    val likeOwnerIds: List<Int>? = null,
+    val likedByMe: Boolean,
+    val attachment: Attachment? = null,
+    val users: List<Users>? = null
+)
+
+data class Users(
+    val id: Int,
+    val userPreview: UserPreview,
+)
+
+data class UserPreview(
+    val name: String,
+    val avatar: String? = null
+)
+
+
+data class Coordinates(
+    val lat: Double,
+    val long: Double,
+)
+
+
+data class Attachment(
+    val url: String,
+    val type: AttachmentType,
+)
+
+enum class AttachmentType {
+    IMAGE,
+    VIDEO,
+    AUDIO,
+}
+
