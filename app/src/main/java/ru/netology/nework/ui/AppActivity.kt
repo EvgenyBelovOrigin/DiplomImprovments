27c61e600580c605a02220f3341d9a9d06557796
Evgeny <e.belov@inbox.ru>
2025-03-25 20:01:32 +0300

    sign out done

--- app/src/main/java/ru/netology/nework/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nework/ui/AppActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.netology.nework.R
+import ru.netology.nework.viewmodel.AuthViewModel
+import ru.netology.nmedia.auth.AppAuth
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AppActivity : AppCompatActivity(R.layout.activity_app) {
+    @Inject
+    lateinit var appAuth: AppAuth
+
+    private val viewModel: AuthViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,13 +28,12 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
             object : MenuProvider {
                 override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                     menuInflater.inflate(R.menu.auth_menu, menu)
-                    menu.setGroupVisible(R.id.authenticated, false)
-                    menu.setGroupVisible(R.id.unauthenticated, true)
-//                    viewModel.data.observe(this@AppActivity) {
-//                        menu.setGroupVisible(R.id.authenticated, viewModel.authenticated)
-//                        menu.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
-//
-//                    }
+
+                    viewModel.data.observe(this@AppActivity) {
+                        menu.setGroupVisible(R.id.authenticated, viewModel.authenticated)
+                        menu.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
+
+                    }
                 }
 
                 override fun onMenuItemSelected(menuItem: MenuItem): Boolean =
@@ -40,10 +48,10 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
                             true
                         }
 
-//                        R.id.signout -> {
-////                            appAuth.clear()
-//                            true
-//                        }
+                        R.id.signout -> {
+                            appAuth.clear()
+                            true
+                        }
 
                         else -> false
                     }
