c4fe3a8b0ee14ccf9861be6f9a264602b37b2d08
Evgeny <e.belov@inbox.ru>
2025-04-17 18:25:50 +0300

    test 3

--- app/src/main/java/ru/netology/nework/adapter/EventsAdapter.kt
+++ app/src/main/java/ru/netology/nework/adapter/EventsAdapter.kt
@@ -25,7 +25,6 @@ interface EventsOnInteractionListener {
     fun onStopAudio() {}
     fun onItemClick(event: Event, position: Int) {}
     fun onVideoPlay(position: Int) {}
-/// test2
 }
 
 class EventsAdapter(
